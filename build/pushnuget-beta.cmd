cd4c7c36f38da2f3acc5d259f453b4498f0a8468
David Lievrouw <dlievrouw2@csc.com>
2016-06-13 14:31:09 +0200

    Update msbuild scripts for NuGet.

---
+++ build/pushnuget-beta.cmd
@@ -0,0 +1,6 @@
+cls
+echo off
+SET DIR=%~dp0%
+IF NOT EXIST "%DIR%log" MKDIR "%DIR%log"
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\MsBuild.exe" /m /v:n "%DIR%Transactions.proj" /p:IsPrerelease="true";PrereleaseName="beta" /target:PushNuGet /logger:FileLogger,Microsoft.Build.Engine;LogFile=%DIR%log/pushnuget.log
+pause
