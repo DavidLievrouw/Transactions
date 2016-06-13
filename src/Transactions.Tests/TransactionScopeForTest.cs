3d09f36dc52a47672cfc640e6e0d925dbb7cd003
David Lievrouw <dlievrouw2@csc.com>
2016-06-13 14:15:27 +0200

    Add unit tests for TransactionScope.

---
+++ src/Transactions.Tests/TransactionScopeForTest.cs
@@ -0,0 +1,25 @@
+﻿namespace Transactions {
+  public class TransactionScopeForTest : TransactionScope {
+    public TransactionScopeForTest() {
+      IsUnwound = false;
+      IsComplete = false;
+      ShouldUnwind = true;
+    }
+
+    public bool ShouldUnwind { get; set; }
+    public bool IsUnwound { get; private set; }
+    public bool IsComplete { get; private set; }
+
+    protected override bool ShouldUnwindScope() {
+      return ShouldUnwind;
+    }
+
+    protected override void UnwindScope() {
+      IsUnwound = true;
+    }
+
+    public override void Complete() {
+      IsComplete = true;
+    }
+  }
+}
---
+++ src/Transactions.Tests/TransactionScopeTests.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+
+namespace Transactions {
+  [TestFixture]
+  public class TransactionScopeTests {
+    [Test]
+    public void UponCreation_SetsAmbientContext() {
+      using (var ctx = new TransactionScopeForTest()) {
+        Assert.That(TransactionScope.Current, Is.EqualTo(ctx));
+      }
+    }
+
+    [Test]
+    public void UponCreation_AddsItemToAmbientContextStack() {
+      Assert.That(TransactionScope.Depth, Is.Zero);
+      using (var ctx1 = new TransactionScopeForTest()) {
+        Assert.That(TransactionScope.Depth, Is.EqualTo(1));
+        using (var ctx2 = new TransactionScopeForTest()) {
+          Assert.That(TransactionScope.Depth, Is.EqualTo(2));
+          Assert.That(TransactionScope.Current, Is.EqualTo(ctx2));
+        }
+      }
+    }
+
+    [Test]
+    public void UponDispose_Unwinds() {
+      TransactionScopeForTest sut;
+      using (sut = new TransactionScopeForTest()) {
+        Assert.That(sut.IsUnwound, Is.False);
+      }
+      Assert.That(sut.IsUnwound, Is.True);
+    }
+
+    [Test]
+    public void UponDispose_DoesNotUnwindIfSpecified() {
+      TransactionScopeForTest sut;
+      using (sut = new TransactionScopeForTest()) {
+        sut.ShouldUnwind = false;
+        Assert.That(sut.IsUnwound, Is.False);
+      }
+      Assert.That(sut.IsUnwound, Is.False);
+    }
+
+    [Test]
+    public void UponDispose_RemovesItemFromAmbientContextStack() {
+      using (var ctx1 = new TransactionScopeForTest()) {
+        using (var ctx2 = new TransactionScopeForTest()) {
+          Assert.That(TransactionScope.Depth, Is.EqualTo(2));
+        }
+        Assert.That(TransactionScope.Depth, Is.EqualTo(1));
+      }
+      Assert.That(TransactionScope.Depth, Is.Zero);
+    }
+  }
+}
